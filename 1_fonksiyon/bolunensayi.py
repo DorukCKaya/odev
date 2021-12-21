89e4fd5c7f56d46582bdc3f16b88f01de87f822f
DorukCKaya <93584078+DorukCKaya@users.noreply.github.com>
2021-12-21 11:40:19 +0300

    Update bolunensayi.py

--- 1_fonksiyon/bolunensayi.py
+++ 1_fonksiyon/bolunensayi.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
 def bolunensayibulma(min_sayi, max_sayi, bolunecek_sayi):
     bolunenler = []
     for i in range(min_sayi, max_sayi+1):
@@ -11,23 +5,4 @@ def bolunensayibulma(min_sayi, max_sayi, bolunecek_sayi):
             bolunenler.append(i)
     return len(bolunenler),  bolunenler
     
-bolunensayibulma(0, 49, 2)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+bolunensayibulma( , , )
